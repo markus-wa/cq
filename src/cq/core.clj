1b45abbef88515e729c40f81b5825d2c671054ce
Markus Walther <m.walther97@gmail.com>
2021-03-20 17:27:31 +0000

    fix hash pipe for graal

--- src/cq/core.clj
+++ src/cq/core.clj
@@ -1,28 +1,20 @@
 (ns cq.core
   (:require [sci.core :as sci]))
 
-(declare ^:dynamic .)
-
 (defn- eval-form
-  [form]
-  (sci/eval-form (sci/init {}) form))
-
-(defn with-dot
-  [x form]
-  (binding [. x
-            *ns* (the-ns 'cq.core)]
-    (eval-form form)))
+  [opts form]
+  (sci/eval-form (sci/init opts) form))
 
 (defn |*
   [form]
   `((fn [x#]
-      (with-dot x#
-        (quote
-         ~form)))))
+      (~'eval-f {:bindings {'. x#}}
+                '~form))))
 
 (defn- thread-last
   [x exps]
-  (eval-form (concat `(->> ~x) exps)))
+  (eval-form {:bindings {'eval-f eval-form}}
+             (concat `(->> ~x) exps)))
 
 (defn query
   [data exps]

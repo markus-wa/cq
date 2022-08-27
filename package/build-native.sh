4ddfccb1c5138fd40c4d418667c78a231a7997eb
Byron Clark <byron@theclarkfamily.name>
2022-08-27 07:57:53 -0600

    Add explicit reflection configuration for native image.
    Resolves #13

--- package/build-native.sh
+++ package/build-native.sh
@@ -6,4 +6,5 @@ native-image --report-unsupported-elements-at-runtime \
              --initialize-at-build-time \
              --no-server \
              -jar ../target/cq.jar \
+             -H:ReflectionConfigurationFiles=./reflection-config.json \
              -H:Name=../target/cq

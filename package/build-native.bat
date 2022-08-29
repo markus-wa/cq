884f7ed3272ce1bfce2f027d7bdcf4779cbced96
Byron Clark <byron@theclarkfamily.name>
2022-08-29 13:22:33 -0600

    Add explicit reflection configuration for msgpack (#14)
    Resolves #13

--- package/build-native.bat
+++ package/build-native.bat
@@ -4,4 +4,5 @@ native-image --report-unsupported-elements-at-runtime ^
              --initialize-at-build-time ^
              --no-server ^
              -jar ../target/cq.jar ^
+             -H:ReflectionConfigurationFiles=./reflection-config.json ^
              -H:Name=../target/cq
